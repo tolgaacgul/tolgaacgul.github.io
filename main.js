7cd558fdce947f884f6b840697787bd82b6fd276
tolgaacgul <tolgaacgul@gmail.com>
2020-10-11 00:00:48 +0300

    ilk işlem

---
+++ main.js
@@ -0,0 +1,10 @@
+var btnGenelBilgi = document.getElementById("idGenelBilgi");
+var btnAileBilgi = document.getElementById("idAileBilgi");
+var btnSGKBilgi = document.getElementById("idSGKBilgi");
+var btnUcretBilgi = document.getElementById("idUcretBilgi");
+var btnDigerBilgi = document.getElementById("idDigerBilgi");
+
+var Personeliframe = document.getElementById("idIframePersonel").
+btnGenelBilgi.onclick = function(){
+    Personeliframe.src = "personelGenelBilgiGir.html"
+}
